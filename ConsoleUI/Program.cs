82c0c42553359afe9fec1d5eded19744dbe66235
Opilane TTHK <opilane@tthk.ee>
2021-11-10 11:58:30 +0200

    first commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleUl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\users\opilane\source\repos\titpe21\FileSystem";
+
+
+            //var files = Directory.GetFiles(rootPath, "*.*", SearchOption.TopDirectoryOnly);
+
+            //   foreach (string file in files)
+            //   {
+            //       //Console.WriteLine(file);
+            //       //Console.WriteLine(Path.GetFileName(file));
+            //       //Console.WriteLine(Path.GetFileNameWithoutExtensions(file));
+            //       //Console.WriteLine(Path.GetFullPath(file));
+            //     //var info = new FileInfo(file);
+            //
+            //     //Console.WriteLine($"{ Path.GetFileName(file)}: { info.Length} bytes");
+            // }
+
+            //   string newPath = @"C:\users\opilane\source\repos\titpe21\FileSystem\SubFolderC\SubSubFolderD";
+            //   bool directoryExists = Directory.Exists(newPath);
+
+            //  if (directoryExists)
+            // {
+            //      Console.WriteLine("The directory does not exists");
+            // }
+            //   else
+            //  {
+            //      Console.WriteLine("The directory does not exists");
+            //      Directory.CreateDirectory(newPath);
+            // }
+
+
+            string[] files = Directory.GetFiles(rootPath);
+            string destinationFolder = @"C:\users\opilane\source\repos\titpe21\FileSystem\SubFolderA\";
+
+            //  foreach (string file in files)
+            // {
+            //      File.Copy(file, $"{destinationFolder}{ Path.GetFileName(file) }", true);
+            // }
+
+            //   for (int i = 0; i < files.Length; i++)
+            // {
+            //      File.Copy(files[i], $"{destinationFolder}{ i }.txt", true);
+            // }
+
+            //  foreach (string file in files)
+            // {
+            //      File.Move(file, $"{destinationFolder}{ Path.GetFileName(file) }");
+            // }
+
+            var Files = Directory.GetFiles(rootPath, "*folder*.*", SearchOption.AllDirectories);
+
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+                //Console.WriteLine(Path.GetFileName(file));
+                //Console.WriteLine(Path.GetFileNameWithoutExtensions(file));
+                //Console.WriteLine(Path.GetFullPath(file));
+                //var info = new FileInfo(file);
+
+                //Console.WriteLine($"{ Path.GetFileName(file) }: { info.Length } bytes");
+
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+}
